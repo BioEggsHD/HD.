094fcdc87720eea5ff471dcbe022ed2cde0c83f2
BioEggsHD <alisup221@gmail.com>
2024-11-11 19:05:45 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,35 @@
-#include "interpreter.cpp" // Include the interpreter
+#include "interpreter.cpp" // Include the interpreter, which includes the parser and lexer
 
 int main() {
-    std::string input = R"(output("Hello, World!"))";
+    // Example input for testing: includes variable assignments and an output statement
+    std::string input = R"(
+        str myVar = "Hello, Interpreter!"
+        bool myVar2 = true
+        int myVar3 = 42
+        output("Execution successful!")
+    )";
+
+    // Initialize lexer, parser, and interpreter
     Lexer lexer(input);
     Parser parser(lexer);
-    auto ast = parser.parse();
+    Interpreter interpreter;
 
-    if (ast) {
-        Interpreter interpreter;
-        interpreter.interpret(ast);
-    } else {
-        std::cerr << "Syntax error!" << std::endl;
+    // Parse and interpret each statement in the input
+    while (true) {
+        auto astNode = parser.parse();
+        if (!astNode) break; // Stop if there is nothing left to parse
+        interpreter.interpret(astNode);
     }
+
+    // Test by printing variable values (these will be retrieved from the symbol table)
+    std::cout << "Value of myVar: ";
+    interpreter.printVariable("myVar");
+
+    std::cout << "Value of myVar2: ";
+    interpreter.printVariable("myVar2");
+
+    std::cout << "Value of myVar3: ";
+    interpreter.printVariable("myVar3");
 
     return 0;
 }
