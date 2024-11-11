29839f1f776f08fd4be08a87ef584eb19992b822
BioEggsHD <alisup221@gmail.com>
2024-11-11 20:19:12 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,26 @@
-#include "interpreter.cpp" // Include the interpreter, which includes the parser and lexer
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "lexer.cpp"
+#include "parser.cpp"
+#include "interpreter.cpp"
 
 int main() {
-    // Example input for testing: includes variable assignments and an output statement
-    std::string input = R"(
-        str myVar = "Hello, Interpreter!"
-        bool myVar2 = true
-        int myVar3 = 42
-        output("Execution successful!")
-    )";
+    // Read the HD-Script source code
+    std::ifstream file("example.hds");
+    std::string code((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
-    // Initialize lexer, parser, and interpreter
-    Lexer lexer(input);
-    Parser parser(lexer);
+    // Create lexer to tokenize the input code
+    Lexer lexer(code);
+    std::vector<Token> tokens = lexer.tokenize();
+
+    // Parse the tokens
+    Parser parser(tokens);
+    parser.parse();
+
+    // Interpret and execute the code
     Interpreter interpreter;
-
-    // Parse and interpret each statement in the input
-    while (true) {
-        auto astNode = parser.parse();
-        if (!astNode) break; // Stop if there is nothing left to parse
-        interpreter.interpret(astNode);
-    }
-
-    // Test by printing variable values (these will be retrieved from the symbol table)
-    std::cout << "Value of myVar: ";
-    interpreter.printVariable("myVar");
-
-    std::cout << "Value of myVar2: ";
-    interpreter.printVariable("myVar2");
-
-    std::cout << "Value of myVar3: ";
-    interpreter.printVariable("myVar3");
+    interpreter.execute(tokens);
 
     return 0;
 }
-
