63ea111e44ccaf451584be47bc833f9a9effa19a
BioEggsHD <alisup221@gmail.com>
2024-11-11 18:51:29 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,18 @@
+#include "interpreter.cpp" // Include the interpreter
+
+int main() {
+    std::string input = R"(output("Hello, World!"))";
+    Lexer lexer(input);
+    Parser parser(lexer);
+    auto ast = parser.parse();
+
+    if (ast) {
+        Interpreter interpreter;
+        interpreter.interpret(ast);
+    } else {
+        std::cerr << "Syntax error!" << std::endl;
+    }
+
+    return 0;
+}
 
