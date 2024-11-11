a5722c73be656297544cf1b35c116280482d3029
BioEggsHD <alisup221@gmail.com>
2024-11-11 18:50:58 +0100

    Update interpreter.cpp

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,1 +1,11 @@
 
+#include "parser.cpp" // Include the parser
+
+class Interpreter {
+public:
+    void interpret(const std::unique_ptr<ASTNode>& node) {
+        if (auto outputNode = dynamic_cast<OutputNode*>(node.get())) {
+            std::cout << outputNode->message << std::endl;
+        }
+    }
+};
